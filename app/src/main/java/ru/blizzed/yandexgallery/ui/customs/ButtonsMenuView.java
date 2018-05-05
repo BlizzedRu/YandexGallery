72232faef30df287152abff3d2051455e8cf5771
BlizzedRu <me@blizzed.ru>
2018-05-05 03:33:39 +0300

    Changing items color and state changing animation provided to ButtonsMenuView

--- app/src/main/java/ru/blizzed/yandexgallery/ui/customs/ButtonsMenuView.java
+++ app/src/main/java/ru/blizzed/yandexgallery/ui/customs/ButtonsMenuView.java
@@ -1,20 +1,38 @@
 package ru.blizzed.yandexgallery.ui.customs;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.support.annotation.ColorInt;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.SparseBooleanArray;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 import ru.blizzed.yandexgallery.R;
 
 public class ButtonsMenuView extends LinearLayout {
 
     private OnItemClickListener listener;
+
+    @ColorInt
+    private int activeColor;
+
+    @ColorInt
+    private int inactiveColor;
+
+    private SparseBooleanArray isActiveArray;
+    private SparseIntArray posToResArray;
 
     public ButtonsMenuView(@NonNull Context context) {
         super(context);
@@ -32,7 +50,11 @@ public class ButtonsMenuView extends LinearLayout {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyle) {
-
+        isActiveArray = new SparseBooleanArray();
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ButtonsMenuView, defStyle, 0);
+        activeColor = array.getColor(R.styleable.ButtonsMenuView_colorActive, getResources().getColor(R.color.colorAccent));
+        inactiveColor = array.getColor(R.styleable.ButtonsMenuView_colorInactive, getResources().getColor(R.color.white));
+        array.recycle();
     }
 
     public void setOnItemClickListener(OnItemClickListener listener) {
@@ -40,6 +62,8 @@ public class ButtonsMenuView extends LinearLayout {
     }
 
     public void setItems(MenuItem[] items) {
+        posToResArray = new SparseIntArray();
+
         if (getChildCount() > 0) {
             removeAllViews();
         }
@@ -54,12 +78,43 @@ public class ButtonsMenuView extends LinearLayout {
             ((ImageView) itemView.findViewById(R.id.img)).setImageDrawable(item.getIcon());
             setListenerTo(itemView, item, position);
             addView(itemView);
+            posToResArray.put(position, item.getItemId());
             position++;
         }
 
         invalidate();
     }
 
+    public void setActive(int position, boolean isActive, boolean animate) {
+        boolean intention = isActive(position) != isActive;
+        if (intention) {
+            isActiveArray.put(position, isActive);
+            ImageView img = getChildAt(position).findViewById(R.id.img);
+
+            int colorIntention = isActive ? activeColor : inactiveColor;
+
+            if (!animate) {
+                setColorTo(img, colorIntention);
+                return;
+            }
+
+            YoYo.with(Techniques.Pulse).duration(500).onEnd((c) -> {
+                setColorTo(img, colorIntention);
+            }).playOn(img);
+        }
+    }
+
+    public boolean isActive(int position) {
+        return isActiveArray.get(position);
+    }
+
+    public int getPositionOf(@IdRes int idRes) {
+        return posToResArray.indexOfValue(idRes);
+    }
+
+    private void setColorTo(ImageView imageView, @ColorInt int color) {
+        imageView.getDrawable().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+    }
     private void setListenerTo(View view, MenuItem item, int position) {
         view.setOnClickListener(v -> {
             if (listener != null) listener.onMenuItemClicked(item, position);
