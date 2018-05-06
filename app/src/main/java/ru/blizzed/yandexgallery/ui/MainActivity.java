9d304d799b3c5055076ddcc8de2df14d5e136d16
BlizzedRu <me@blizzed.ru>
2018-05-06 09:46:01 +0300

    MainActivity correct fragments behavior

--- app/src/main/java/ru/blizzed/yandexgallery/ui/MainActivity.java
+++ app/src/main/java/ru/blizzed/yandexgallery/ui/MainActivity.java
@@ -13,44 +13,35 @@ import ru.blizzed.yandexgallery.ui.screens.files.FilesPage;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FavoritePage favoritePage = new FavoritePage();
-    private FilesPage filesPage = new FilesPage();
-    private FeedPage feedPage = new FeedPage();
+    private static final String KEY_TAG = "fragment_tag";
 
     private String currentFragmentTag;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = item -> {
         switch (item.getItemId()) {
             case R.id.navigation_feed:
-                openFragment(feedPage, FeedPage.TAG);
+                openFragment(FeedPage.TAG);
                 return true;
             case R.id.navigation_files:
-                openFragment(filesPage, FilesPage.TAG);
+                openFragment(FilesPage.TAG);
                 return true;
             case R.id.navigation_favorite:
-                openFragment(favoritePage, FavoritePage.TAG);
+                openFragment(FavoritePage.TAG);
                 return true;
         }
         return false;
     };
 
-    private void openFragment(Fragment fragment, String tag) {
+    private void openFragment(String tag) {
         if (tag.equals(currentFragmentTag)) return;
 
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
-
         if (currentFragmentTag != null) {
             transaction.hide(getFragmentManager().findFragmentByTag(currentFragmentTag));
-            if (getFragmentManager().findFragmentByTag(tag) == null)
-                transaction.add(R.id.pagesContainer, fragment, tag);
-            else transaction.show(fragment);
-        } else {
-            transaction.add(R.id.pagesContainer, fragment, tag);
         }
+        transaction.show(getFragmentManager().findFragmentByTag(tag)).commit();
 
         currentFragmentTag = tag;
-
-        transaction.commit();
     }
 
     @Override
@@ -58,10 +49,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState == null) {
+            addFragments();
+        }
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_feed);
+    }
 
+    private void addFragments() {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        addFragment(new FeedPage(), FeedPage.TAG, transaction);
+        addFragment(new FilesPage(), FilesPage.TAG, transaction);
+        addFragment(new FavoritePage(), FavoritePage.TAG, transaction);
+
+        transaction.commit();
+        getFragmentManager().executePendingTransactions();
+    }
+
+    private void addFragment(Fragment fragment, String tag, FragmentTransaction transaction) {
+        transaction.add(R.id.pagesContainer, fragment, tag).hide(fragment);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY_TAG, currentFragmentTag);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        currentFragmentTag = savedInstanceState.getString(KEY_TAG, null);
     }
 
 }
